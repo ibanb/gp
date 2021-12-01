9e4e3afc45f23e5af3ca53cf8bfd8b505c375eef
ibonburger <ibonburger@mail.ru>
2021-12-01 20:58:02 +0300

    less 32 done

---
+++ lesson_32/less_3/mendeleev.rb
@@ -0,0 +1,39 @@
+# encoding: utf-8
+#
+# Программа «Таблица Менделеева», которая показывает информацию об элементе
+#
+# (с) goodprogrammer.ru
+
+# Этот код необходим только при использовании русских букв на Windows
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+# Подключим библиотеку json
+require 'json'
+
+# Как и в предыдущем задании, прочитаем таблицу элементов в ассоциативный массив
+file = File.read("#{__dir__}/elements.json", encoding: 'UTF-8')
+elements = JSON.parse(file)
+
+# Напишем пользователю, какие элементы у нас есть
+puts "У нас всего элементов: #{elements.keys.size}"
+puts elements.keys.join(', ')
+
+# Спросим пользователя, о каком элементе он хочет узнать
+puts 'О каком элементе хотите узнать?'
+element = gets.chomp
+
+# Если такой элемент есть в нашем хэше, покажем информацию о нём
+if elements.key?(element)
+  elements[element].each do |item|
+    p "#{item[0]} - #{item[1]}"
+  end
+else
+  puts 'Извините, про такой элемент мы еще не знаем.'
+end
